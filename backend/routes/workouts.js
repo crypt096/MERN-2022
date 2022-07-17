076ae4a7d027a499524a507128dbb2d9cd6f335e
Aleksandar Cvjetean <31933549+crypt096@users.noreply.github.com>
2022-07-17 17:28:45 +0200

    Update workouts.js

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -13,20 +13,7 @@ router.get('/:id', (req, res) => {
 })
 
 // POST new workout
-router.post('/', async (req, res) => {
-  const { title, load, reps } = req.body;
-
-  try {
-    const workout = await Workout.create({
-      title,
-      load,
-      reps
-    }) 
-    res.status(200).json(workout);
-  } catch (error) {
-    res.status(400).json({ error: error.message });
-  }
-})
+router.post('/', creteWorkout);
 
 // UPDATE a workout
 router.patch('/:id', (req, res) => {
