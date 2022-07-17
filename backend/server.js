37f97c5497c4c991d2f0e12b67d4dfef933f7648
Aleksandar Cvjetean <31933549+crypt096@users.noreply.github.com>
2022-07-17 12:22:45 +0200

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,20 @@
-const express = require('express');
 require('dotenv').config();
+
+const express = require('express');
+const workoutRoutes = require('./routes/workouts');
 
 const app = express();
 
+// Middleware
 app.use((req, res, next) => {
   console.log(req.path, req.method);
   next();
 })
 
-app.get('/', (req, res) => {
-  res.json({ message: 'Hello World' });
-})
+// Routes
+app.use(workoutRoutes);
 
+// Server listening
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on port ${process.env.PORT}`);
 })
