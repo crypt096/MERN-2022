d728d5b5cb39b9676cee2a2da1bc5fd7d436546c
Aleksandar Cvjetean <31933549+crypt096@users.noreply.github.com>
2022-07-17 17:37:34 +0200

    Update workoutController.js

--- backend/controllers/workoutController.js
+++ backend/controllers/workoutController.js
@@ -14,11 +14,10 @@ const getWorkouts = async (req, res) => {
 const getWorkout = async (req, res) => {
   const { id } = req.params;
 
-  try {
-    const workout = await Workout.findById(id);
-    res.status(200).json(workout);
-  } catch (error) {
-    res.status(400).json({ error: error.message });
+  const workout = await Workout.findById(id);
+
+  if(!workout) {
+    return res.status(404).json({ error: 'Workout not found' });
   }
 }
 
