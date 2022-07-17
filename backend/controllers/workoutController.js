5fa2a51d7157095a6c7ed3a0a9a6cb08b85e9fca
Aleksandar Cvjetean <31933549+crypt096@users.noreply.github.com>
2022-07-17 17:33:16 +0200

    Update workoutController.js

--- backend/controllers/workoutController.js
+++ backend/controllers/workoutController.js
@@ -3,7 +3,7 @@ const Workout = require('../models/workoutModel');
 // Get all workouts
 const getWorkouts = async (req, res) => {
   try {
-    const workouts = await Workout.find();
+    const workouts = await Workout.find().sort({ createdAt: -1 });
     res.status(200).json(workouts);
   } catch (error) {
     res.status(400).json({ error: error.message });
