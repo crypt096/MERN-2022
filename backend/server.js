f02a38eb80ed06891a27dd41fd3da84a135d5a4e
Aleksandar Cvjetean <31933549+crypt096@users.noreply.github.com>
2022-07-17 12:09:36 +0200

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,10 @@ const app = express();
 
 const PORT = 5000;
 
+app.get('/', (req, res) => {
+  res.json({ message: 'Hello World' });
+})
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 })
