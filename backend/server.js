95ce8557a4eda45d3ebc53ae2cbc07c6700818c9
Aleksandar Cvjetean <31933549+crypt096@users.noreply.github.com>
2022-07-17 12:12:31 +0200

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,12 @@
 const express = require('express');
+require('dotenv').config();
 
 const app = express();
-
-const PORT = 5000;
 
 app.get('/', (req, res) => {
   res.json({ message: 'Hello World' });
 })
 
-app.listen(PORT, () => {
+app.listen(process.env.PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 })
