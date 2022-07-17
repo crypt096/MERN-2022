ce8af9d36ed557dfbee72ff7fdc6142b8509f186
Aleksandar Cvjetean <31933549+crypt096@users.noreply.github.com>
2022-07-17 12:24:09 +0200

    Update workouts.js

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', () => {})
+// Get ALL workouts
+router.get('/', (req, res) => {
+  res.json({mssg: 'GET All workouts'});
+})
 
 module.exports = router;
