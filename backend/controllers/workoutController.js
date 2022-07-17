01d98a43a15b29d458d42a85f9f30454ff3fd48b
Aleksandar Cvjetean <31933549+crypt096@users.noreply.github.com>
2022-07-17 17:28:00 +0200

    Update workoutController.js

--- backend/controllers/workoutController.js
+++ backend/controllers/workoutController.js
@@ -5,6 +5,20 @@ const Workout = require('../models/workout');
 // Get single workout
 
 // Create workout
+const createWorkout = async (req, res) => {
+  const { title, load, reps } = req.body;
+
+  try {
+    const workout = await Workout.create({
+      title,
+      load,
+      reps
+    }) 
+    res.status(200).json(workout);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+}
 
 // Update workout
 
